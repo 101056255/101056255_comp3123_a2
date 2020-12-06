83f384af03e54fe6dc8ac1ddd794470326749b65
101056255 <edwardphilip30@gmail.com>
2020-12-05 19:47:17 -0500

    Added date and date function

--- src/App.js
+++ src/App.js
@@ -21,7 +21,9 @@ export default class App extends Component{
                             main: undefined
                         }
                     ],
-                    name: undefined
+                    name: undefined,
+                    dt: undefined,
+                    timezone: undefined
                 }
 
             }
@@ -33,11 +35,25 @@ export default class App extends Component{
             .then(res => this.setState({result: res.data})).then(() => console.log(this.state.result))
     }
 
+
+    timeConverter(UNIX_timestamp){
+        const a = new Date(UNIX_timestamp * 1000);
+        const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+        const year = a.getFullYear();
+        const month = months[a.getMonth()];
+        const day = days[a.getDay()]
+        const date = a.getDate();
+        const hour = a.getHours();
+        const min = a.getMinutes();
+        return day + ', ' + date + ' ' + month + ' ' + year + ' ' + hour + ':' + min;
+    }
+
     render(){
       return (
           <div className="App">
               <header className="App-header">
-                  <Card bg="secondary" style={{ width: '18rem' }} text="dark" border="danger">
+                  <Card bg="secondary" style={{ width: '25rem' }} text="dark" border="danger">
                       <Card.Img variant="top" src={`http://openweathermap.org/img/wn/${this.state.result.weather[0].icon}@2x.png`} />
                       <Card.Body>
                           <Card.Title>{this.state.result.name}</Card.Title>
@@ -49,6 +65,9 @@ export default class App extends Component{
                           </Card.Text>
                           <Card.Text>
                              {this.state.result.weather[0].main}
+                          </Card.Text>
+                          <Card.Text>
+                              {this.timeConverter(this.state.result.dt)}
                           </Card.Text>
                       </Card.Body>
                   </Card>
